78fb384f16278ddedaf24192ca20a63356157232
rebelBeard <rfm144@gmail.com>
2017-04-29 20:13:38 -0400

    coded store, actions, and reducers

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PhotoGrid extends Component {
+	constructor() {
+		super()
+
+	}
+
+	render() {
+		return(
+			<div className="photo-grid">
+				I'm the photo grid				
+			</div>
+		)
+	}
+}
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+	constructor() {
+		super()
+
+	}
+
+	render() {
+		return(
+			<div className="single-photo">
+				I'm the single 
+			</div>
+		)
+	}
+}
+
+export default Single;
